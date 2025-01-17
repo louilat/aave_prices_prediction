d9197a7f7c97cf74fd37fe770bf2eef65795e72f
louilat <lmm.latournerie@gmail.com>
2025-01-17 15:16:24 +0100

    add events etl

---
+++ events_etl.py
@@ -0,0 +1,101 @@
+"""ETL for extracting events data"""
+
+import boto3
+from datetime import datetime, timedelta, timezone
+from dotenv import load_dotenv
+import os
+import io
+from events.events_extraction_functions import fetch_events, clean_events_data
+from utils.logger import Logger
+
+logger = Logger()
+
+load_dotenv()
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
+API_SECRET_KEY = os.getenv("API_SECRET_KEY")
+
+
+# Run parameters
+output_path = "aave-data/events-dev/"
+file_name = "events_data"
+year = 2024
+months = [1]
+version_2 = False
+# events_list = ["deposit", "borrow", "repay", "redeemUnderlying"]
+events_list = ["liquidationCall"]
+
+
+if version_2:
+    api_endpoint = f"https://gateway.thegraph.com/api/{API_SECRET_KEY}/subgraphs/id/8wR23o1zkS4gpLqLNU4kG3JHYVucqGyopL5utGxP2q1N"
+else:
+    api_endpoint = f"https://gateway.thegraph.com/api/{API_SECRET_KEY}/subgraphs/id/Cd2gEDVeqnjBn1hSeqFMitw8Q1iiyV9FYUZkLNRcL87g"
+
+
+client_s3 = boto3.client(
+    "s3",
+    endpoint_url="https://" + "minio.lab.sspcloud.fr",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    aws_session_token=AWS_SESSION_TOKEN,
+)
+
+
+for month in months:
+    # Compute month dates
+    start_date = datetime(year, month, 1, tzinfo=timezone.utc)
+    next_date = start_date + timedelta(days=32)
+    end_date = datetime(next_date.year, next_date.month, 1, tzinfo=timezone.utc)
+    timestamp_min = datetime.timestamp(start_date)
+    timestamp_max = datetime.timestamp(end_date)
+    logger.log(f"min_date: {start_date}, max_date: {end_date}")
+    logger.log(
+        f"Starting data extraction with version_2 = {version_2} timestamp_min = {timestamp_min}, timestamp_max = {timestamp_max}"
+    )
+
+    for event_name in events_list:
+        logger.log(f"   --> Fetching {event_name} events...")
+        raw_events = fetch_events(
+            api_endpoint=api_endpoint,
+            size=1000,
+            max_queries=300,
+            timestamp_min=timestamp_min,
+            timestamp_max=timestamp_max,
+            event=event_name,
+            verbose=True,
+        )
+
+        logger.log(len(raw_events))
+
+        logger.log(f"   --> Cleaning {event_name} events...")
+        clean_events = clean_events_data(
+            event_name=event_name,
+            events_data=raw_events,
+            logger=logger,
+        )
+        logger.log(f"   --> INFO: Found {len(clean_events)} events")
+
+        try:
+            csv_buffer = io.StringIO()
+            clean_events.to_csv(csv_buffer, index=False)
+            client_s3.put_object(
+                Body=csv_buffer.getvalue(),
+                Bucket="llatournerie",
+                Key=output_path
+                + f"{event_name}/"
+                + file_name
+                + f"_{event_name}_{year}-{month}"
+                + ".csv",
+            )
+            logger.log(f"   --> Outputs successfully generated at {output_path}")
+        except Exception as e:
+            logger.log(f"Failed to upload files to s3, with error: {e}")
+
+client_s3.put_object(
+    Body=logger.buffer.getvalue(),
+    Bucket="llatournerie",
+    Key=output_path + f"logfile_{year}.log",
+)
+
+logger.log("Done !")
--- src/events/events_extraction_functions.py
+++ src/events/events_extraction_functions.py
@@ -1,8 +1,14 @@
 """Functions for extracting events data"""
 
 from ..utils.utils import run_query
+from ..utils.logger import Logger
+import pandas as pd
+from pandas import DataFrame
 
-def extract_deposits(api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int) -> dict:
+
+def extract_deposit(
+    api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int
+) -> dict:
     query = f"""\u007b
         supplies(
             first: {size},
@@ -36,10 +42,12 @@ def extract_deposits(api_endpoint: str, size: int, offset: int, timestamp_min: i
             timestamp
         \u007d
     \u007d"""
-    return run_query(api=api_endpoint, query=query)
+    return run_query(api=api_endpoint, query=query)["data"]["supplies"]
 
 
-def extract_borrows(api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int) -> dict:
+def extract_borrow(
+    api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int
+) -> dict:
     query = f"""\u007b
         borrows(
             first: {size},
@@ -73,10 +81,12 @@ def extract_borrows(api_endpoint: str, size: int, offset: int, timestamp_min: in
             timestamp
         \u007d
     \u007d"""
-    return run_query(api=api_endpoint, query=query)
+    return run_query(api=api_endpoint, query=query)["data"]["borrows"]
 
 
-def extract_redeemUnderlying(api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int) -> dict:
+def extract_redeemUnderlying(
+    api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int
+) -> dict:
     query = f"""\u007b
         redeemUnderlyings(
             first: {size},
@@ -110,10 +120,12 @@ def extract_redeemUnderlying(api_endpoint: str, size: int, offset: int, timestam
             timestamp
         \u007d
     \u007d"""
-    return run_query(api=api_endpoint, query=query)
+    return run_query(api=api_endpoint, query=query)["data"]["redeemUnderlyings"]
 
 
-def extract_usageAsCollateral(api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int) -> dict:
+def extract_usageAsCollateral(
+    api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int
+) -> dict:
     query = f"""\u007b
         usageAsCollaterals(
             first: {size},
@@ -144,10 +156,12 @@ def extract_usageAsCollateral(api_endpoint: str, size: int, offset: int, timesta
             timestamp
         \u007d
     \u007d"""
-    return run_query(api=api_endpoint, query=query)
+    return run_query(api=api_endpoint, query=query)["data"]["usageAsCollaterals"]
 
 
-def extract_repay(api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int) -> dict:
+def extract_repay(
+    api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int
+) -> dict:
     query = f"""\u007b
         repays(
             first: {size},
@@ -182,10 +196,12 @@ def extract_repay(api_endpoint: str, size: int, offset: int, timestamp_min: int,
             timestamp
         \u007d
     \u007d"""
-    return run_query(api=api_endpoint, query=query)
+    return run_query(api=api_endpoint, query=query)["data"]["repays"]
 
 
-def extract_flashloan(api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int) -> dict:
+def extract_flashloan(
+    api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int
+) -> dict:
     query = f"""\u007b
         flashLoans(
             first: {size},
@@ -217,10 +233,12 @@ def extract_flashloan(api_endpoint: str, size: int, offset: int, timestamp_min: 
             timestamp
         \u007d
     \u007d"""
-    return run_query(api=api_endpoint, query=query)
+    return run_query(api=api_endpoint, query=query)["data"]["flashLoans"]
 
 
-def extract_liquidationCall(api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int) -> dict:
+def extract_liquidationCall(
+    api_endpoint: str, size: int, offset: int, timestamp_min: int, timestamp_max: int
+) -> dict:
     query = f"""\u007b
         liquidationCalls(
             first: {size},
@@ -259,4 +277,97 @@ def extract_liquidationCall(api_endpoint: str, size: int, offset: int, timestamp
             timestamp
         \u007d
     \u007d"""
-    return run_query(api=api_endpoint, query=query)
+    return run_query(api=api_endpoint, query=query)["data"]["liquidationCalls"]
+
+
+def fetch_events(
+    api_endpoint: str,
+    size: int,
+    max_queries: int,
+    timestamp_min: int,
+    timestamp_max: int,
+    event: str,
+    verbose: bool = False,
+) -> DataFrame:
+    event_functions = {
+        "deposit": extract_deposit,
+        "redeemUnderlying": extract_redeemUnderlying,
+        "borrow": extract_borrow,
+        "usageAsCollateral": extract_usageAsCollateral,
+        "repay": extract_repay,
+        "flashloan": extract_flashloan,
+        "liquidationCall": extract_liquidationCall,
+    }
+
+    try:
+        extraction_function = event_functions[event]
+    except KeyError:
+        raise ValueError(f"Unknown event type: {event}")
+
+    events_data = pd.DataFrame()
+    for iter in range(max_queries):
+        if verbose:
+            if iter % 10 == 0:
+                print(f"      [Iteration {iter + 1}/{max_queries}]")
+        query_output = extraction_function(
+            api_endpoint=api_endpoint,
+            size=size,
+            offset=iter * size,
+            timestamp_min=timestamp_min,
+            timestamp_max=timestamp_max,
+        )
+        current_events = pd.json_normalize(query_output)
+        if len(current_events) == 0:
+            print("All data has been already extracted")
+            break
+        events_data = pd.concat((events_data, current_events))
+    return events_data
+
+
+def clean_events_data(
+    event_name: str, events_data: DataFrame, logger: Logger
+) -> DataFrame:
+    rename_columns = {
+        "pool.pool": "pool",
+        "user.id": "user_id",
+        "caller.id": "caller_id",
+        "to.id": "to_id",
+        "reserve.underlyingAsset": "underlying_asset",
+        "reserve.name": "reserve_name",
+        "reserve.decimals": "reserve_decimals",
+        "repayer.id": "repayer_id",
+        "initiator.id": "initiator_id",
+        "collateralReserve.decimals": "collateral_reserve_decimals",
+        "principalReserve.decimals": "principal_reserve_decimals",
+        "collateralReserve.name": "collateral_reserve_name",
+        "principalReserve.name": "principal_reserve_name",
+    }
+    clean_data = events_data.rename(columns=rename_columns)
+
+    if event_name not in ["liquidationCall", "usageAsCollateral"]:
+        clean_data.reserve_decimals = clean_data.reserve_decimals.astype(int)
+        clean_data.amount = (
+            clean_data.amount.apply(int) / 10**clean_data.reserve_decimals
+        )
+
+    if event_name == "liquidationCall":
+        clean_data.collateral_reserve_decimals = (
+            clean_data.collateral_reserve_decimals.astype(int)
+        )
+        clean_data.principal_reserve_decimals = (
+            clean_data.principal_reserve_decimals.astype(int)
+        )
+        clean_data.collateralAmount = (
+            clean_data.collateralAmount.apply(int)
+            / 10**clean_data.collateral_reserve_decimals
+        )
+        clean_data.principalAmount = (
+            clean_data.principalAmount.apply(int)
+            / 10**clean_data.principal_reserve_decimals
+        )
+
+    clean_data_ = clean_data.drop_duplicates()
+
+    logger.log(f"      --> Found {len(clean_data) - len(clean_data_)} duplicates")
+
+    return clean_data_
